f11a90992754d1d26fb50fa7cb54fde15e21ef01
D. Davis <D. Davis>
2016-07-27 13:28:01 -0400

    working project:

---
+++ server/models/definition.js
@@ -0,0 +1,29 @@
+// definition model needs description, logtype, and an owner
+module.exports = function(sequelize, DataTypes) {
+
+	var Definition = sequelize.define('definition', {
+		description: DataTypes.STRING,
+		logType: DataTypes.STRING,
+		owner: DataTypes.INTEGER
+	});
+
+	return Definition;
+};
+/*
+	{
+			definition: "run 5k",
+			logType: "byTime",
+			owner: 1 // user id of who created it
+		
+		}
+	},
+
+	{,
+
+		definition: {
+			description: "pullups",
+			logType: "reps",
+			owner: 6
+		}
+	}
+*/
